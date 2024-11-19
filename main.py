24f459392106bcc1017e4e2ce0860eca456c7ea3
Ethan Hunt <122260799+hunteth000@users.noreply.github.com>
2024-11-18 19:18:33 -0500

    Update 11/18

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import atexit
 import shutil
 import os
 
+
 def cleanup_temp_dir():
     if os.path.exists(globals.TEMP_DIR):
         shutil.rmtree(globals.TEMP_DIR)
+
 
 if __name__ == "__main__":
     globals.setup_temp_dir()
